7de99da874cce524dcc31d5489eec3e841aca180
Marco Canales <canalesmarco1@gmail.com>
2021-05-31 02:40:46 -0400

    Generar estructuras de laravel

---
+++ web/zgames/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view("/","registrar_consola")->name("registrar_consolas")
+
+Route::view("/registrar_juego","registrar_juego")->name("registrar_juegos")
+
+Route::view("/ver_consolas","ver_consolas")->name("ver_consolas")
+
+Route::view("/ver_juegos","ver_juegos")->name("ver_juegos")
+
+
+
+/* Route::get('/', function () {
+    return view('registrar_juego');
+});
+
+Route::get("/registrar_juego", function () {
+    return view("registrar_juego");
+});
+
+Route::get("/registrar_consola", function () {
+    return view("registrar_consola");
+});
+
+Route::get("/ver_consolas", function () {
+    return view("ver_consolas");
+});
+
+Route::get("/ver_juegos", function () {
+    return view("ver_juegos");
+}); */
